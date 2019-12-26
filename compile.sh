d8e00ab34fa129879d13d6704237f9d05098a08e
sleonard <sleonard@student.42.fr>
2019-12-26 19:32:34 +0300

    fixed back compile.sh

--- compile.sh
+++ compile.sh
@@ -11,8 +11,8 @@ if [[ ! -f "libft/Makefile" ]]; then
   git submodule init
   git submodule update --merge --remote
 fi
-cd libft
-make -j 4
+cd libft || echo "can't find libft directory!"
+make
 cd ..
 mkdir build
 cd build || echo "can't make build directory!"
